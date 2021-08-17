9cea3076b1805b316d77dd8114d39914725e0c7b
Kavish Rizvi <rizvikavish205@gmail.com>
2021-08-17 14:29:08 +0530

    first commit

---
+++ src/Java8/Lambda/FIExample.java
@@ -0,0 +1,6 @@
+package Java8.Lambda;
+
+@FunctionalInterface
+public interface FIExample {
+    String getDetails();
+}
---
+++ src/Java8/Lambda/LambdaExpressionsDemo.java
@@ -0,0 +1,4 @@
+package Java8.Lambda;
+
+public class LambdaExpressionsDemo {
+}
